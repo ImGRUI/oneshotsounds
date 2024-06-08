6d8d0a3b687d23bef2bc1b5238734a008ca04117
ImGRUI <91282421+ImGRUI@users.noreply.github.com>
2024-06-08 21:17:42 +0500

    Update Files  - Remove Legacy Mixins  - Cleanup en_us.json, sounds.json  - Remove Legacy Files

--- src/main/java/com/hattolo/consolesounds/ConsoleSoundsConfig.java
+++ src/main/java/com/hattolo/consolesounds/ConsoleSoundsConfig.java
@@ -10,15 +10,6 @@ public class ConsoleSoundsConfig implements ConfigData {
     @Comment("Enables the sound you hear when you hover over buttons.")
     public boolean enableHoverSounds = true;
 
-    @Comment("Enables the click sound you hear when you click on a slot on an in-game menu, like a crafting table or your inventory.")
-    public boolean enableInGameClickSounds = false;
-
-    @Comment("Enables the pop sound you hear when you craft something (Legacy).")
-    public boolean enableCraftingSounds = false;
-
-    @Comment("Enables the sound you hear when you use a slider (Legacy).")
-    public boolean enableSliderSounds = false;
-
     @Comment("Plays the click sound whenever you open the pause menu. You might want to disable this if you set the game to auto pause on focus loss.")
     public boolean playSoundOnPauseMenu = true;
 
@@ -35,34 +26,22 @@ public class ConsoleSoundsConfig implements ConfigData {
 
     @Comment("Volume when you hover over buttons.")
     @ConfigEntry.BoundedDiscrete(min = 0, max = 100)
-    public int hoverVolume = 100;
-
-    @Comment("Volume when you click on a slot on an in-game menu.")
-    @ConfigEntry.BoundedDiscrete(min = 0, max = 100)
-    public int  inGameClickVolume = 100;
-
-    @Comment("Volume when you craft something.")
-    @ConfigEntry.BoundedDiscrete(min = 0, max = 100)
-    public int craftingVolume = 100;
-
-    @Comment("Volume when you use a slider.")
-    @ConfigEntry.BoundedDiscrete(min = 0, max = 100)
-    public int sliderVolume = 100;
+    public int hoverVolume = 45;
 
     @Comment("Volume when you open the pause menu.")
     @ConfigEntry.BoundedDiscrete(min = 0, max = 100)
-    public int onPauseMenuVolume = 70;
+    public int onPauseMenuVolume = 25;
 
     @Comment("Volume when you open an in-game menu.")
     @ConfigEntry.BoundedDiscrete(min = 0, max = 100)
-    public int inGameMenuVolume = 70;
+    public int inGameMenuVolume = 25;
 
     @Comment("Volume when you close a menu.")
     @ConfigEntry.BoundedDiscrete(min = 0, max = 100)
-    public int menuExitVolume = 70;
+    public int menuExitVolume = 25;
 
     @Comment("Volume when you close an in-game menu.")
     @ConfigEntry.BoundedDiscrete(min = 0, max = 100)
-    public int onInGameMenuExitVolume = 70;
+    public int onInGameMenuExitVolume = 25;
 }
 
--- src/main/java/com/hattolo/consolesounds/ConsoleSoundsSounds.java
+++ src/main/java/com/hattolo/consolesounds/ConsoleSoundsSounds.java
@@ -11,10 +11,6 @@ public final class ConsoleSoundsSounds {
 
 	public static final SoundEvent UI_BACK = register("ui_back");
 
-	public static final SoundEvent UI_SCROLL = register("ui_scroll");
-
-	public static final SoundEvent UI_FAIL = register("ui_fail");
-
 	private static SoundEvent register(String name) {
         Identifier id = ConsoleSoundsClient.id(name);
         return Registry.register(Registries.SOUND_EVENT, id, SoundEvent.of(id));
